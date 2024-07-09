93aff76bb8d47fb1b308fd2952d8200b6979a99c
eri <k.erii0341115@gmail.com>
2024-07-09 11:46:22 +0900

    fix:ドライバーの修正

--- app/jobs/async_log_job.rb
+++ app/jobs/async_log_job.rb
@@ -2,7 +2,7 @@ class AsyncLogJob < ApplicationJob
   queue_as :async_log
 
   def perform(user)
-    UserMailer.with(user: user).welcome_email.deliver_leter
+    UserMailer.with(user: user).welcome_email.deliver_now
 
   # def perform(message: "hello") # message引数を追加
   #   AsyncLog.create!(message: message) # データベースに保存する
--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,7 @@
 class UserMailer < ApplicationMailer
-  def welcome_email
-    @user = params[:user]
-    mail(to: @user.email, subject: 'Welcome to Our App')
+  def welcome
+    @name = params[:name] # params: 呼び出し元から渡されたデータ
+    # インスタンス変数に代入すると次のビューで使える
+    mail(to: params[:to], subject: "登録完了") # メール送信
   end
-  # def welcome
-  #   @name = params[:name] # params: 呼び出し元から渡されたデータ
-  #   # インスタンス変数に代入すると次のビューで使える
-  #   mail(to: params[:to], subject: "登録完了") # メール送信
-  # end
 end
